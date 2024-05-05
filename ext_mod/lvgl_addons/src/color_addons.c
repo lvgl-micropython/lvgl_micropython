f0eecb359ab4160f667101cc52f0907d446a0b8f
Kevin Schlosser <kdschlosser@users.noreply.github.com>
2024-05-05 00:57:07 -0600

    changes to the color addons

--- ext_mod/lvgl_addons/src/color_addons.c
+++ ext_mod/lvgl_addons/src/color_addons.c
@@ -18,20 +18,20 @@ float soft_atan2( float y, float x )
     static const float b = 0.596227f;
 
     // Extract the sign bits
-    uint32_t ux_s  = sign_mask & (uint32_t &)x;
-    uint32_t uy_s  = sign_mask & (uint32_t &)y;
+    uint32_t ux_s  = sign_mask & ((uint32_t) &)x;
+    uint32_t uy_s  = sign_mask & ((uint32_t) &)y;
 
     // Determine the quadrant offset
     float q = (float)( ( ~ux_s & uy_s ) >> 29 | ux_s >> 30 );
 
     // Calculate the arctangent in the first quadrant
-    float bxy_a = fabs( b * x * y );
+    float bxy_a = (float)fabs( b * x * y );
     float num = bxy_a + y * y;
     float atan_1q =  num / ( x * x + bxy_a + num );
 
     // Translate it to the proper quadrant
-    uint32_t uatan_2q = (ux_s ^ uy_s) | (uint32_t &)atan_1q;
-    return q + (float &)uatan_2q;
+    uint32_t uatan_2q = (ux_s ^ uy_s) | ((uint32_t) &)atan_1q;
+    return q + ((float) &)uatan_2q;
 }
 
 #endif /* MP_SOFT_ATAN */
