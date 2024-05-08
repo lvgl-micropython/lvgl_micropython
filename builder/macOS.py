6c6407652038858cd8dbe80f9fe096624be3f15d
Kevin Schlosser <kdschlosser@users.noreply.github.com>
2024-05-08 01:16:22 -0600

    working on macOS build

--- builder/macOS.py
+++ builder/macOS.py
@@ -65,7 +65,8 @@ def build_commands(_, extra_args, script_dir, lv_cflags, board):
     unix_cmd.extend([
         f'LV_CFLAGS="{lv_cflags}"',
         f'LV_PORT=unix',
-        f'USER_C_MODULES="{script_dir}/ext_mod"'
+        f'USER_C_MODULES="{script_dir}/ext_mod"',
+        f'CFLAGS=-Wno-unused-function'
     ])
     unix_cmd.extend(extra_args)
 
