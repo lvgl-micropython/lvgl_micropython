677a2437c46e605e370d8f1e0416af1d0d8be026
Kevin Schlosser <kdschlosser@users.noreply.github.com>
2024-06-05 01:35:51 -0600

    fixes macos compile error

--- ext_mod/lcd_utils/include/remap.h
+++ ext_mod/lcd_utils/include/remap.h
@@ -3,5 +3,5 @@
 
 #ifndef __REMAP_H__
     #define __REMAP_H__
-    extern mp_obj_fun_builtin_var_t mp_lcd_utils_remap_obj;
+    extern const mp_obj_fun_builtin_var_t mp_lcd_utils_remap_obj;
 #endif /* __REMAP_H__ */
