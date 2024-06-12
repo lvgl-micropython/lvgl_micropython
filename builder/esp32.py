101addc6624eb1a1df60714f31628bce9e2fa6a7
Kevin Schlosser <kdschlosser@users.noreply.github.com>
2024-06-12 09:39:55 -0600

    working on being able to share the SPI Bus

--- builder/esp32.py
+++ builder/esp32.py
@@ -352,7 +352,7 @@ def build_commands(_, extra_args, script_dir, lv_cflags, ___):
         f'LV_PORT=esp32',
         f'BOARD={board}',
         'USER_C_MODULES=../../../../../ext_mod/micropython.cmake',
-        f'-DMICROPY_CPP_INC_EXTRA={script_dir}/micropy_updates/common'
+        f'"IDFPY_FLAGS=-DMICROPY_CPP_INC_EXTRA={script_dir}/micropy_updates/common"'
     ])
 
     esp_cmd.extend(extra_args)
