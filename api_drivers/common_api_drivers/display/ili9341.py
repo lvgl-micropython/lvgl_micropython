371a3ee01162d3092e39f0898f6f407b545f5978
Kevin Schlosser <kdschlosser@users.noreply.github.com>
2024-06-17 21:47:36 -0600

    reverts some of the changes made to the ili9341 driver.
    I had hard coded `color_space` to BGR when it turns out that there are variants that use RGB. So I am defaulting `color_space` to RGB which is what is done across the board for all other display drivers. The users that have the BGR type displays IC's will need to set this parameter.
    
    Fixes #55

--- api_drivers/common_api_drivers/display/ili9341.py
+++ api_drivers/common_api_drivers/display/ili9341.py
@@ -40,51 +40,6 @@ BYTE_ORDER_BGR = display_driver_framework.BYTE_ORDER_BGR
 
 class ILI9341(display_driver_framework.DisplayDriver):
 
-    def __init__(
-        self,
-        data_bus,
-        display_width,
-        display_height,
-        frame_buffer1=None,
-        frame_buffer2=None,
-        reset_pin=None,
-        reset_state=STATE_LOW,
-        power_pin=None,
-        power_on_state=STATE_HIGH,
-        backlight_pin=None,
-        backlight_on_state=STATE_HIGH,
-        offset_x=0,
-        offset_y=0,
-        color_byte_order=BYTE_ORDER_RGB,  # NOQA
-        color_space=lv.COLOR_FORMAT.RGB565,
-        rgb565_byte_swap=False
-    ):
-
-        if (
-            color_space == lv.COLOR_FORMAT.RGB565 and
-            isinstance(data_bus, (lcd_bus.SPIBus, lcd_bus.I80Bus))
-        ):
-            rgb565_byte_swap = True
-
-        super().__init__(
-            data_bus=data_bus,
-            display_width=display_width,
-            display_height=display_height,
-            frame_buffer1=frame_buffer1,
-            frame_buffer2=frame_buffer2,
-            reset_pin=reset_pin,
-            reset_state=reset_state,
-            power_pin=power_pin,
-            power_on_state=power_on_state,
-            backlight_pin=backlight_pin,
-            backlight_on_state=backlight_on_state,
-            offset_x=offset_x,
-            offset_y=offset_y,
-            color_byte_order=BYTE_ORDER_BGR,
-            color_space=color_space,
-            rgb565_byte_swap=rgb565_byte_swap
-        )
-
     def init(self, sequence=1):
         param_buf = bytearray(15)
         param_mv = memoryview(param_buf)
