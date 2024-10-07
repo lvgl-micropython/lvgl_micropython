a1534329058796092793912343b3595135ddb6d6
Kevin Schlosser <kdschlosser@users.noreply.github.com>
2024-10-07 01:37:25 -0600

    blah

---
+++ ext_mod/threading/micropython.cmake
@@ -0,0 +1,20 @@
+# Create an INTERFACE library for our C module.
+
+add_library(usermod_lcd_utils INTERFACE)
+
+set(LCD_UTILS_INCLUDES)
+
+set(LCD_UTILS_SOURCES
+    ${CMAKE_CURRENT_LIST_DIR}/src/lcd_utils.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/remap.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/binary_float.c
+)
+
+# Add our source files to the lib
+target_sources(usermod_lcd_utils INTERFACE ${LCD_UTILS_SOURCES})
+
+# Add include directories.
+target_include_directories(usermod_lcd_utils INTERFACE ${LCD_UTILS_INCLUDES})
+
+# Link our INTERFACE library to the usermod target.
+target_link_libraries(usermod INTERFACE usermod_lcd_utils)
