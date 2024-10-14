2b498a7758c5e19382ec70654a8fe53861db001f
Kevin Schlosser <kdschlosser@users.noreply.github.com>
2024-10-13 19:42:03 -0600

    Update st7796.py

--- api_drivers/common_api_drivers/display/st7796/st7796.py
+++ api_drivers/common_api_drivers/display/st7796/st7796.py
@@ -9,39 +9,16 @@ STATE_PWM = display_driver_framework.STATE_PWM
 BYTE_ORDER_RGB = display_driver_framework.BYTE_ORDER_RGB
 BYTE_ORDER_BGR = display_driver_framework.BYTE_ORDER_BGR
 
-_MADCTL_MH = const(0x04)  # Refresh 0=Left to Right, 1=Right to Left
-_MADCTL_BGR = const(0x08)  # BGR color order
-_MADCTL_ML = const(0x10)  # Refresh 0=Top to Bottom, 1=Bottom to Top
-_MADCTL_MV = const(0x20)  # 0=Normal, 1=Row/column exchange
-_MADCTL_MX = const(0x40)  # 0=Left to Right, 1=Right to Left
-_MADCTL_MY = const(0x80)  # 0=Top to Bottom, 1=Bottom to Top
+_MADCTL_MV = const(0x20)
+_MADCTL_MX = const(0x40)
+_MADCTL_MY = const(0x80)
 
 
 class ST7796(display_driver_framework.DisplayDriver):
-    # The st7795 display controller has an internal framebuffer
-    # arranged in 320 x 480
-    # configuration. Physical displays with pixel sizes less than
-    # 320 x 480 must supply a start_x and
-    # start_y argument to indicate where the physical display begins
-    # relative to the start of the
-    # display controllers internal framebuffer.
-
-    # this display driver supports RGB565 and also RGB666. RGB666 is going to
-    # use twice as much memory as the RGB565. It is also going to slow down the
-    # frame rate by 1/3, This is becasue of the extra byte of data that needs
-    # to get sent. To use RGB666 the color depth MUST be set to 32.
-    # so when compiling
-    # make sure to have LV_COLOR_DEPTH=32 set in LVFLAGS when you call make.
-    # For RGB565 you need to have LV_COLOR_DEPTH=16
-
-    # the reason why we use a 32 bit color depth is because of how the data gets
-    # written. The entire 8 bits for each byte gets sent. The controller simply
-    # ignores the lowest 2 bits in the byte to make it a 6 bit color channel
-    # We just have to tell lvgl that we want to use
 
     _ORIENTATION_TABLE = (
-        0x0,
-        _MADCTL_MV | _MADCTL_MX,
-        _MADCTL_MY | _MADCTL_MX,
-        _MADCTL_MV | _MADCTL_MY
+        _MADCTL_MX,
+        _MADCTL_MV | _MADCTL_MY | _MADCTL_MX,
+        _MADCTL_MY,
+        _MADCTL_MV
     )
