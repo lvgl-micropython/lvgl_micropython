c08083acfd24c1f3e0b06d7fda33226a65aa80cb
Kevin Schlosser <kdschlosser@users.noreply.github.com>
2024-12-01 01:07:55 -0700

    Fixes macOS compile error

--- ext_mod/lcd_bus/micropython.mk
+++ ext_mod/lcd_bus/micropython.mk
@@ -32,6 +32,7 @@ else
     ifneq (,$(findstring macOS, $(LV_PORT)))
         # -F/Library/Frameworks -framework SDL2
         CFLAGS_USERMOD += -DMP_PORT_UNIX=1
+        CFLAGS_USERMOD += -Wno-incompatible-pointer-types-discards-qualifiers
         CFLAGS_USERMOD += -I/Library/Frameworks/SDL2.framework/Headers/SDL2
         CFLAGS_USERMOD += $(shell sdl2-config --cflags)
     	LDFLAGS_USERMOD += $(shell sdl2-config --libs)
