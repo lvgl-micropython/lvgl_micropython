01702bb020785d6c62f55e23f4c30f164afc651a
Kevin Schlosser <kdschlosser@users.noreply.github.com>
2025-02-10 21:34:04 -0700

    adds rm67162 display driver

---
+++ api_drivers/common_api_drivers/display/rm67162/_rm67162_init.py
@@ -0,0 +1,126 @@
+from micropython import const  # NOQA
+import lvgl as lv
+import time
+
+
+_SLPOUT = const(0x11)
+_SWRESET = const(0x01)
+_INVON = const(0x21)
+_INVOFF = const(0x20)
+
+_DISPOFF = const(0x28)
+_DISPON = const(0x29)
+
+
+_CASET = const(0x2A)
+_RASET = const(0x2B)
+
+_RAMWR = const(0x2C)
+_TEOFF = const(0x34)
+_MADCTL = const(0x36)
+
+# Set Color Enhance
+_IMGEHCCTR = const(0x58)
+
+# Set Color Enhance 1
+_CESLRCTR_L = const(0x5A)
+_CESLRCTR_H = const(0x5B)
+
+_COLMOD = const(0x3A)
+_RGB565 = const(0x75)  # 16bit/pixel (65,536 colors)
+_RGB888 = const(0x77)  # 24bit/pixel (16.7M colors)
+
+# Write Display Brightness
+# 0 to 255
+_WRDISBV = const(0x51)
+
+# Write Display Control
+_WRCTRLD = const(0x53)
+_BCTRL = const(0x20)
+_DD = const(0x08)
+
+# Set Dual SPI Mode
+_SetDSPIMode = const(0xC4)
+_DSPI_EN = const(0x21)
+
+# CMD Mode Switch
+# 0x00 == user mode
+_CMDMode = const(0xFE)
+
+# High Brightness Mode
+# 0x00 disable
+# 0x02 enable
+_SETHBM = const(0xB0)
+
+
+def init(self):
+    self._br_val = 0
+
+    buf = self._param_buf
+    mv = self._param_mv
+
+    time.sleep_ms(120)  # NOQA
+    self.set_params(_SWRESET)
+    time.sleep_ms(120)  # NOQA
+
+    num_lanes = self._data_bus.get_lane_count()
+    if num_lanes == 2:
+        buf[0] = _DSPI_EN
+        self.set_params(_SetDSPIMode, mv[:1])
+    elif num_lanes not in (1, 8):
+        raise RuntimeError('display only supports I8080 8 lane, SPI and DUAL SPI')
+
+    self.set_params(_SLPOUT)
+    time.sleep_ms(120)  # NOQA
+
+    buf[0] = 0x05
+    self.set_params(_CMDMode, mv[:1])
+
+    self.set_params(0x05, mv[:1])
+
+    buf[0] = 0x01
+    self.set_params(_CMDMode, mv[:1])
+
+    buf[0] = 0x25
+    self.set_params(0x73, mv[:1])
+
+    buf[0] = 0x00
+    self.set_params(_CMDMode, mv[:1])
+
+    self.set_params(_TEOFF)
+
+    buf[0] = (
+        self._madctl(
+            self._color_byte_order,
+            self._ORIENTATION_TABLE  # NOQA
+        )
+    )
+    self.set_params(_MADCTL, mv[:1])
+
+    color_size = lv.color_format_get_size(self._color_space)
+    if color_size == 2:  # NOQA
+        buf[0] = _RGB565
+    elif color_size == 3:
+        buf[0] = _RGB888
+    else:
+        raise RuntimeError(
+            f'{self.__class__.__name__} IC only supports '
+            'RGB565 and RGB888'
+        )
+
+    self.set_params(_COLMOD, mv[:1])
+
+    buf[0] = _BCTRL | _DD
+    self.set_params(_WRCTRLD, mv[:1])
+
+    buf[0] = 0x7F
+    self.set_params(_CESLRCTR_H, mv[:1])
+
+    buf[0] = 0xFF
+    self.set_params(_CESLRCTR_L, mv[:1])
+
+    buf[0] = 0x00
+    self.set_params(_IMGEHCCTR, mv[:1])
+    self.set_params(_WRDISBV, mv[:1])
+    self.set_params(_SETHBM, mv[:1])
+    self.set_params(_DISPON)
---
+++ api_drivers/common_api_drivers/display/rm67162/rm67162.py
@@ -0,0 +1,145 @@
+from micropython import const  # NOQA
+import display_driver_framework
+import lvgl as lv
+
+
+STATE_HIGH = display_driver_framework.STATE_HIGH
+STATE_LOW = display_driver_framework.STATE_LOW
+STATE_PWM = display_driver_framework.STATE_PWM
+
+BYTE_ORDER_RGB = display_driver_framework.BYTE_ORDER_RGB
+BYTE_ORDER_BGR = display_driver_framework.BYTE_ORDER_BGR
+
+
+_MADCTL_MY = const(0x01)  # mirror y
+_MADCTL_MX = const(0x02)  # mirror x
+_MADCTL_MV = const(0x20)  # x, y = y, x
+
+
+# Write Display Brightness
+# 0 to 255
+_WRDISBV = const(0x51)
+
+# Set Color Enhance
+_IMGEHCCTR = const(0x58)
+_SLR_EN = const(0x04)  # Sunlight Readable
+# Sunlight Readable Level values are 0, 1, and 3
+
+# Set Color Enhance 1
+_CESLRCTR_L = const(0x5A)
+_CESLRCTR_H = const(0x5B)
+
+# High Brightness Mode
+# 0x00 disable
+# 0x02 enable
+_SETHBM = const(0xB0)
+_HBM_EN = const(0x02)
+
+
+class RM67162(display_driver_framework.DisplayDriver):
+    _ORIENTATION_TABLE = (
+        0x00,
+        _MADCTL_MV,
+        _MADCTL_MY | _MADCTL_MX,
+        _MADCTL_MY | _MADCTL_MX | _MADCTL_MV
+    )
+
+    def __init__(
+        self,
+        data_bus,
+        display_width,
+        display_height,
+        frame_buffer1=None,
+        frame_buffer2=None,
+        reset_pin=None,
+        reset_state=STATE_HIGH,
+        power_pin=None,
+        power_on_state=STATE_HIGH,
+        backlight_pin=None,
+        backlight_on_state=STATE_HIGH,
+        offset_x=0,
+        offset_y=0,
+        color_byte_order=BYTE_ORDER_RGB,
+        color_space=lv.COLOR_FORMAT.RGB888,  # NOQA
+        rgb565_byte_swap=False
+    ):
+
+        self.__brightness = 0
+        self.__sunlight = 0
+        self.__ambient = 0x7FFF
+        self.__high_brightness = 0x00
+
+        super().__init__(
+            data_bus=data_bus,
+            display_width=display_width,
+            display_height=display_height,
+            frame_buffer1=frame_buffer1,
+            frame_buffer2=frame_buffer2,
+            reset_pin=reset_pin,
+            reset_state=reset_state,
+            power_pin=power_pin,
+            power_on_state=power_on_state,
+            backlight_pin=backlight_pin,
+            backlight_on_state=backlight_on_state,
+            offset_x=offset_x,
+            offset_y=offset_y,
+            color_byte_order=color_byte_order,
+            color_space=color_space,
+            rgb565_byte_swap=rgb565_byte_swap
+        )
+
+    def set_brightness(self, val):
+        # convert the value from a 0.0 - 100.0 scale to a 0 - 255 scale
+        val = int(val * 255 / 100)
+        # clamp the balue so it is 0 - 255
+        val = max(min(val, 255), 0)
+
+        self.__brightness = val
+        self._param_buf[0] = val
+
+        self.set_params(_WRDISBV, self._param_mv[:1])
+
+    def get_brightness(self):
+        return round(self.__brightness / 255.0 * 100.0, 2)
+
+    def set_ambient_light_level(self, val):
+        val = int(val * 65535 / 100)
+        val = max(min(val, 65535), 0)
+
+        self.__ambient = val
+        self._param_buf[0] = val >> 8
+        self.set_params(_CESLRCTR_H, self._param_mv[:1])
+
+        self._param_buf[0] = val & 0xFF
+        self.set_params(_CESLRCTR_L, self._param_mv[:1])
+
+    def get_amblent_light_level(self):
+        return round(self.__ambient / 65535.0 * 100.0, 2)
+
+    def set_sunlight_enhance(self, val):
+        if val == -1:
+            self.__sunlight &= ~_SLR_EN
+        else:
+            val = min(val, 3)
+            self.__sunlight = _SLR_EN | val
+
+        self._param_buf[0] = self.__sunlight
+        self.set_params(_IMGEHCCTR, self._param_mv[:1])
+
+    def get_sunlight_enhance(self):
+        if self.__sunlight & _SLR_EN:
+            return self.__sunlight & ~_SLR_EN
+
+        return -1
+
+    def set_high_brightness(self, val):
+        if val:
+            self.__high_brightness |= _HBM_EN
+        else:
+            self.__high_brightness &= ~_HBM_EN
+
+        self._param_buf[0] = self.__high_brightness
+        self.set_params(_SETHBM, self._param_mv[:1])
+
+    def get_high_brightness(self):
+        return bool(self.__high_brightness & _HBM_EN)
