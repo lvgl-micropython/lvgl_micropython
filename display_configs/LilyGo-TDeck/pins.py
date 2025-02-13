b52ced5546d252374a4fc9e039a95329a828b174
Kevin Schlosser <kdschlosser@users.noreply.github.com>
2025-02-12 23:51:00 -0700

    lilygo tdeck

--- display_configs/LilyGo-TDeck/pins.py
+++ display_configs/LilyGo-TDeck/pins.py
@@ -1,8 +1,3 @@
-
-_I2S_WS = const(5)
-_I2S_DA = const(6)
-_I2S_BCK = const(7)
-
 _C3_INT = const(46)
 
 _ES7210_DIN = const(14)
@@ -10,6 +5,9 @@ _ES2710_LRCK = const(21)
 _ES2710_SCLK = const(47)
 _ES2710_MCLK = const(48)
 
+_I2S_WS = const(5)
+_I2S_DA = const(6)
+_I2S_BCK = const(7)
 
 _GPIO0 = const(0)
 
