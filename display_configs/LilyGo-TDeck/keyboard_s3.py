95077875ec7771de6b9c3a210157469b4ba8f73f
Kevin Schlosser <kdschlosser@users.noreply.github.com>
2025-02-27 09:55:41 -0700

    fixes keypad driver, a better way

--- display_configs/LilyGo-TDeck/keyboard_s3.py
+++ display_configs/LilyGo-TDeck/keyboard_s3.py
@@ -2,13 +2,68 @@
 
 import lvgl as lv  # NOQA
 import keypad_framework
+from micropython import const  # NOQA
+import lcd_utils
+
+_LILYGO_KB_BRIGHTNESS_CMD = const(0x01)
+_LILYGO_KB_ALT_B_BRIGHTNESS_CMD = const(0x02)
 
 
 class Keyboard(keypad_framework.KeypadDriver):
     def __init__(self, device, debug=False):  # NOQA
         self._device = device
         self._debug = debug
+        self._brightness = 0
+        self._brightness_default = 127
+
         super().__init__()
+
+    def set_default_brioghtness(self, value):
+        value = lcd_utils.remap(float(value), 0.0, 100.0, 30.0, 255.0)
+        value = int(round(value))
+
+        # clamp value at 29 - 255
+        if value <= 30:
+            value = 0
+
+        if value > 255:
+            value = 255
+
+        self._brightness_default = value
+
+        value = bytearray([value])
+        self._device.write_mem(_LILYGO_KB_ALT_B_BRIGHTNESS_CMD, value)
+
+    def get_brightness_default(self):
+        value = self._brightness_default
+        if value == 0:
+            value = 30
+
+        value = lcd_utils.remap(float(value), 30.0, 255.0, 0.0, 100.0)
+        return round(value, 1)
+
+    def set_brightness(self, value):
+        value = lcd_utils.remap(float(value), 0.0, 100.0, 30.0, 255.0)
+        value = int(round(value))
+
+        # clamp value at 29 - 255
+        if value <= 30:
+            value = 0
+
+        if value > 255:
+            value = 255
+
+        self._brightness = value
+        value = bytearray([value])
+        self._device.write_mem(_LILYGO_KB_BRIGHTNESS_CMD, value)
+
+    def get_brightness(self):
+        value = self._brightness
+        if value == 0:
+            value = 30
+
+        value = lcd_utils.remap(float(value), 30.0, 255.0, 0.0, 100.0)
+        return round(value, 1)
 
     def _get_key(self):
         # lv.KEY.ESC = 0x1B
@@ -17,7 +72,7 @@ class Keyboard(keypad_framework.KeypadDriver):
         # lv.KEY.PREV = 0x0B
         # lv.KEY.HOME = 0x02
         # lv.KEY.END = 0x03
-        key = bytearray(self._device.read(1))[0]
+        key = bytearray(self._device.read_mem(0x00, num_bytes=1))[0]
         if key == 0x00:  # no key
             return None
         elif key == 0x08:  # backspace
