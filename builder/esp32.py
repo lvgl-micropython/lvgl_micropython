e99e3f628fb44baac2f9a5efb338b16814978995
Kevin Schlosser <kdschlosser@users.noreply.github.com>
2025-02-28 18:52:51 -0700

    add python 3.13 to supported versions..
    Fixes #305

--- builder/esp32.py
+++ builder/esp32.py
@@ -1469,7 +1469,7 @@ def compile(*args):  # NOQA
 
             espressif_path = os.path.expanduser('~/.espressif')
 
-            for ver in ('3.8', '3.9', '3.10', '3.11', '3.12'):
+            for ver in ('3.8', '3.9', '3.10', '3.11', '3.12', '3.13'):
                 python_path = (
                     f'{espressif_path}/python_env/'
                     f'idf{IDF_VER[:-2]}_py{ver}_env/bin'
