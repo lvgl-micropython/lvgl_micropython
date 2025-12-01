900f3be611ff4aded2e0ac571bbaa4b920811659
Kevin Schlosser <kdschlosser@users.noreply.github.com>
2025-11-30 22:36:15 -0700

    updating MicroPython version

--- builder/__init__.py
+++ builder/__init__.py
@@ -482,7 +482,7 @@ def get_micropython():
         'git submodule update --init --depth=1 -- lib/micropython',
     ]
     print()
-    print('collecting MicroPython 1.26.1')
+    print('collecting MicroPython 1.27.0 (master)')
     result, _ = spawn(cmd_, spinner=True)
     if result != 0:
         sys.exit(result)
